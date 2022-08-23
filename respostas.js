986703363e65d3cd4425f9a106f031dc247b476b
devrobsonaraujo <108498541+devrobsonaraujo@users.noreply.github.com>
2022-08-22 22:03:56 -0300

    commit initial
    commit initial

---
+++ respostas.js
@@ -0,0 +1,114 @@
+function resposta_Cap1(num) {
+    switch(num) {
+        case 1: 
+            document.getElementById("letraA").innerHTML = "O efeito fotoelétrico é a ejeção de elétróns por alguns materiais quando iluminado por frequências específicas de ondas eletromagnéticas.";
+            break;
+            
+        case 2:      
+            document.getElementById("letraB").innerHTML = "8,5eV";
+            
+            break;
+        case 3:
+            document.getElementById("letraC").innerHTML = "10,20eV";
+            break;
+        case 4:
+            document.getElementById("letraD").innerHTML = "Fóton";
+            break;
+        case 'a':
+            document.getElementById("letra_a").innerHTML = "1,03.10^25";
+            break;
+        case 'b':
+            document.getElementById("letra_b").innerHTML = "1,56,10^33";
+            break;
+        case 'c':
+            document.getElementById("letra_c").innerHTML = "3,7.10^65";
+            break;
+        case 'd':
+            document.getElementById("letra_d").innerHTML = "1,36.10^3";
+            break;
+        case 'e':
+            document.getElementById("letra_e").innerHTML = "9,5.10^5";
+            break;        
+    }
+}
+
+function resposta_Cap2(num) {
+    switch(num) {
+        case 1:
+            document.getElementById("letraA").innerHTML = "É uma grandeza vetorial, pois apresenta módulo, direção e sentido, sendo uma propriedade quântica intríseca das partículas elementares, assim como massa e carga. É uma espécie de orientação da partícula quando imersa num campo magnético.";
+            break;
+        case 2:
+            document.getElementById("letraB").innerHTML = " 3p4 ";
+            break;
+        case 3:
+            document.getElementById("letraC").innerHTML = "n=2, l=1, m=+1 e s=+1/2";
+            break;
+        case 4:
+            document.getElementById("letraD").innerHTML = "25 elétrons para cima e 75 elétrons para baixo.";
+            break;
+        case 5:
+            document.getElementById("letraE").innerHTML = "raiz8h";
+            break;
+    }
+}
+
+function resposta_Cap3(num) {
+    switch(num) {
+        case 1:
+            document.getElementById("letraA").innerHTML = "Buracos negros são estágios finais de estrelas que possuem massa maior do que 25 massas do sol e em determinadas regiões do universo existem efeitos intensos devido à curvatura do espaço-tempo e em consequência, qualquer corpo ou radiação não conseguirá sair destas regiões, nem a luz consegue escapar.";
+            break;
+        case 2:
+            document.getElementById("letraB").innerHTML = "Estrelas de neutrôns.";
+            break;
+        case 3:
+            document.getElementById("letraC").innerHTML = "Kerr";
+            break;
+        case 4:
+            document.getElementById("letraD").innerHTML = "2,2 * 10^-9K";
+            break;
+        case 5:
+            document.getElementById("letraE").innerHTML = "É proporcional à área de um buraco negro.";
+            break;
+    }
+}
+
+function resposta_Cap4(num) {
+    switch(num) {
+        case 1:
+            document.getElementById("letraA").innerHTML = "M(8,2) e N(2,6)";
+            break;
+        case 2:
+            document.getElementById("letraB").innerHTML = "t = 1 e 15";
+            break;
+        case 3:
+            document.getElementById("letraC").innerHTML = "12 u.v + 30 u.a = 42";
+            break;
+        case 4:
+            document.getElementById("letraD").innerHTML = "Sim, pois satisfaz a desigualdade triangular.";
+            break;
+        case 5:
+            document.getElementById("letraE").innerHTML = "5/6(3raiz2 + raiz10)"
+            break
+    }
+}
+
+function resposta_Cap5(num) {
+    switch(num) {
+        case 1:
+            document.getElementById("letraA").innerHTML = "Resposta do estudante.";
+            break;
+        case 2:
+            document.getElementById("letraB").innerHTML = " ";
+            break;
+        case 3:
+            document.getElementById("letraC").innerHTML = "(A) a energia observada por Pedro é duas vezes maior do que a energia observada por João.";
+            break;
+        case 4:
+            document.getElementById("letraD").innerHTML = "(C) q = 12p";
+            break;
+        case 5:
+            document.getElementById("letraE").innerHTML = "(A) ln 2";
+            break;
+    }
+}
+
